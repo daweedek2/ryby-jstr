4fe51a3e0a3e507a55311e109d8b80c9d47c93b0
David Kostka <dkostka@eurofunk.com>
2023-07-23 09:35:28 +0200

    Support catch date and time

--- src/main/java/cz/kostka/rybyjstr/dto/NewCatchDTO.java
+++ src/main/java/cz/kostka/rybyjstr/dto/NewCatchDTO.java
@@ -1,13 +1,13 @@
 package cz.kostka.rybyjstr.dto;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public record NewCatchDTO(
         Long fishTypeId,
         Long hunterId,
         Integer size,
         Long weight,
-        LocalTime time,
+        LocalDateTime time,
         String note) {
     public static NewCatchDTO empty() {
         return new NewCatchDTO(null, null, null, null, null, null);
