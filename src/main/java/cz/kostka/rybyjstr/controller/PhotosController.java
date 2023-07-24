85d0718563add4089a74bbc4ef80ab37ea7d4fcd
David Kostka <dkostka@eurofunk.com>
2023-07-24 23:08:02 +0200

    Support catch overview with photos

---
+++ src/main/java/cz/kostka/rybyjstr/controller/PhotosController.java
@@ -0,0 +1,26 @@
+package cz.kostka.rybyjstr.controller;
+
+import cz.kostka.rybyjstr.service.CatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/photos")
+public class PhotosController {
+
+    private final CatchService catchService;
+
+    @Autowired
+    public PhotosController(final CatchService catchService) {
+        this.catchService = catchService;
+    }
+
+    @GetMapping
+    public String viewAllPhotos(final Model model) {
+        model.addAttribute("allCatches", catchService.getAllCatches());
+        return "photos";
+    }
+}
